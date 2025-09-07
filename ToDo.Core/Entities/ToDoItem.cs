328af44e3a1efac8c316c20589b471fef133b781
PJ <pj@paweljanicki.pl>
2025-09-08 00:37:23 +0200

    Modified property type

--- ToDo.Core/Entities/ToDoItem.cs
+++ ToDo.Core/Entities/ToDoItem.cs
@@ -3,7 +3,7 @@
     public class ToDoItem
     {
         public int Id { get; set; }
-        public DateOnly ExpiryDate { get; set; }
+        public DateTime ExpiryDate { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public short CompletePercentage { get; set; }
